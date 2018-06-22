fbea2716c098616ba640880305f45256b093ec54
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-22 16:05:44 -0600

    Added script for learning how to use OpenCV with static images.
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

---
+++ pyimagesearch/chapter_3/load_display_save.py
@@ -0,0 +1,18 @@
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+print("Width: {} pixels".format(image.shape[1]))
+print("Height: {} pixels".format(image.shape[0]))
+print("Channels:", image.shape[2])
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+cv2.imwrite("newimage.jpg", image)
+
+
