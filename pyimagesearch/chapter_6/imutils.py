050617f6cbf0cd44d410e7e82a4fef6d1f810ce6
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-30 14:00:36 -0600

    Added convenience function for rotating and scaling an image.
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- pyimagesearch/chapter_6/imutils.py
+++ pyimagesearch/chapter_6/imutils.py
@@ -2,6 +2,31 @@ import numpy as np
 import cv2
 
 
+def rotate(image, angle, center=None, scale=1.0):
+    """
+    Convenience function to rotate and optionally scale an image.
+    :param image: Image to be manipulated.
+    :param angle: Angle to rotate the image.
+    :param center: Center coordinate from which to perform the
+    rotation.
+    :param scale: Amount to scale the image.
+    :return: Return the rotated image.
+    """
+    # Grab the dimensions of the image
+    (h, w) = image.shape[:2]
+
+    # If the center is None, initialize it as the center of the image
+    if center is None:
+        center = (w / 2, h / 2)
+
+    # Perform the rotation
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+
+    # Return the rotated image
+    return rotated
+
+
 def translate(image, x, y):
     M = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
--- pyimagesearch/chapter_6/rotate.py
+++ pyimagesearch/chapter_6/rotate.py
@@ -12,14 +12,28 @@ cv2.imshow("Original", image)
 cv2.waitKey(0)
 
 (h, w) = image.shape[:2]
-center = (w // 2, h // 2)
+(cX, cY) = (w / 2, h / 2)
 
-M = cv2.getRotationMatrix2D(center, 45, 1.0)
+M = cv2.getRotationMatrix2D((cX, cY), 45, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h))
 cv2.imshow("Rotated by 45 Degrees", rotated)
 cv2.waitKey(0)
 
-M = cv2.getRotationMatrix2D(center, -90, 1.0)
+M = cv2.getRotationMatrix2D((cX, cY), -90, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h))
 cv2.imshow("Rotated by -90 Degrees", rotated)
+cv2.waitKey(0)
+
+# Rotate our image around an arbitrary point rather than the center
+M = cv2.getRotationMatrix2D((cX -50, cY - 50), 45, 1.0)
+rotated = cv2.warpAffine(image, M, (w, h))
+cv2.imshow("Rotated by Offset & 45 Degrees", rotated)
+cv2.waitKey(0)
+
+"""
+Finally, let's use our helper function in imutils to rotate by 180
+degrees (flipping it upside down)
+"""
+rotated = imutils.rotate(image, 180)
+cv2.imshow("Rotated by 180 Degrees", rotated)
 cv2.waitKey(0)
