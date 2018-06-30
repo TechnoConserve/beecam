502ab5a8763c5d4c06015a72009fc82510876605
Avery Uslaner <uslaner.avery@gmail.com>
2018-06-30 17:23:26 -0600

    Added loop to explore the effects of various interpolation methods.
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- pyimagesearch/chapter_6/resizing.py
+++ pyimagesearch/chapter_6/resizing.py
@@ -45,3 +45,19 @@ our target width or height and have it take care of the rest for us.
 resized = imutils.resize(image, width=100)
 cv2.imshow("Resized via Function", resized)
 cv2.waitKey(0)
+
+methods = [
+    ("cv2.INTER_NEAREST", cv2.INTER_NEAREST),
+    ("cv2.INTER_LINEAR", cv2.INTER_LINEAR),
+    ("cv2.INTER_AREA", cv2.INTER_AREA),
+    ("cv2.INTER_CUBIC", cv2.INTER_CUBIC),
+    ("cv2.INTER_LANCZOS4", cv2.INTER_LANCZOS4),
+]
+
+# loop over the interpolation methods
+for (name, method) in methods:
+    # Increase the size of the image by 3x using the current
+    # interpolation method
+    resized = imutils.resize(image, width=image.shape[1] * 3, inter=method)
+    cv2.imshow("Method: {}".format(name), resized)
+    cv2.waitKey(0)
