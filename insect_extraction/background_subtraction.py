f20588f3370c12ddf602954914bd80a7ca0cf3d9
Avery Uslaner <uslaner.avery@gmail.com>
2018-07-03 14:56:24 -0600

    Updated background subtraction script to use FileVideoStream from the umutils package which breaks the image decoding into its own thread to prevent blocking when OpenCV reads each frame.
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- insect_extraction/background_subtraction.py
+++ insect_extraction/background_subtraction.py
@@ -1,16 +1,20 @@
+import time
+
+from imutils.video import FPS, FileVideoStream
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture('test30fps.h264')
+cap = FileVideoStream('test30fps.h264').start()
+time.sleep(1.0)
 fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
 
-while True:
-    ret, frame = cap.read()
+while cap.more():
+    frame = cap.read()
     fgmask = fgbg.apply(frame)
     cv.imshow('Original Frame', frame)
     cv.imshow('Masked Frame', fgmask)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+cap.stop()
 cv.destroyAllWindows()
