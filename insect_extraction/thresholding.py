b4aadc5318e8c66ebe8c3aa1e3e2f79eae7d7831
Avery Uslaner <uslaner.avery@gmail.com>
2018-07-03 12:41:49 -0600

    Added examples of using binarization for processing images.
    Signed-off-by: Avery Uslaner <uslaner.avery@gmail.com>

--- insect_extraction/thresholding.py
+++ insect_extraction/thresholding.py
@@ -33,7 +33,45 @@ titles = ['Original Image', 'Global Thresholding (v = 205)',
 images = [img, th1, th2, th3]
 
 for i in range(4):
-    plt.subplot(2, 2, i+1), plt.imshow(images[i], 'gray')
+    plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
+plt.show()
+
+"""
+Binarization
+
+Binarization is meant for bimodal images, which doesn't work
+very well for our image since the pixel intensities exist
+along a gradient. The corners are darker and more out of focus
+compared to the center of the photo so this method ends up
+not working very well.
+"""
+img = cv2.imread('../pyimagesearch/images/bees.jpg', 0)
+
+# global thresholding
+ret1, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+# Otsu's thresholding
+ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# Otsu's thresholding after Gaussian filtering
+blur = cv2.GaussianBlur(img, (5, 5), 0)
+ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+# plot all the images and their histograms
+images = [img, 0, th1,
+          img, 0, th2,
+          blur, 0, th3]
+titles = ['Original Noisy Image', 'Histogram', 'Global Thresholding (v=127)',
+          'Original Noisy Image', 'Histogram', "Otsu's Thresholding",
+          'Gaussian filtered Image', 'Histogram', "Otsu's Thresholding"]
+
+for i in range(3):
+    plt.subplot(3, 3, i * 3 + 1), plt.imshow(images[i * 3], 'gray')
+    plt.title(titles[i * 3]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 2), plt.hist(images[i * 3].ravel(), 256)
+    plt.title(titles[i * 3 + 1]), plt.xticks([]), plt.yticks([])
+    plt.subplot(3, 3, i * 3 + 3), plt.imshow(images[i * 3 + 2], 'gray')
+    plt.title(titles[i * 3 + 2]), plt.xticks([]), plt.yticks([])
 plt.show()
